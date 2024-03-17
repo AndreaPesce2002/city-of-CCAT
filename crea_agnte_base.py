a94755b8893e0bebc1f9dfb9febc5b66d3a911ec
AndreaPesce2002 <andrea.pesce.lavoro@gmail.com>
2024-03-17 13:22:06 +0100

    update agente_base_forked

--- crea_agnte_base.py
+++ crea_agnte_base.py
@@ -10,10 +10,11 @@ from packaging import version
 agent_folder_path = "agenti"
 
 # URL del repository GitHub
-github_repo_url = "https://github.com/cheshire-cat-ai/core.git"
+#github_repo_url = "https://github.com/cheshire-cat-ai/core.git"
+github_repo_url = "https://github.com/AndreaPesce2002/core_city_of_CCAT.git"
 
 # Nome della cartella scaricata
-downloaded_folder_name = "agente_base"
+downloaded_folder_name = "agente_base_2"
 
 # Percorso della cartella dei file modificati
 modified_files_path = "fileModificati"
@@ -22,7 +23,8 @@ destination_path = os.path.join(agent_folder_path, downloaded_folder_name)
 
 if os.path.exists(destination_path):
     # Gestisci la situazione come preferisci, ad esempio eliminando la cartella esistente
-    shutil.rmtree(destination_path)
+    #shutil.rmtree(destination_path)
+    subprocess.run(["sudo","rm","-fr", "./agente_base"], cwd=os.path.join(agent_folder_path), check=True)
 
 repo = Repo.clone_from(github_repo_url, destination_path)
 
@@ -32,32 +34,6 @@ os.rename(os.path.join(agent_folder_path, downloaded_folder_name), os.path.join(
 # Sostituisci la cartella plugins
 shutil.rmtree(os.path.join(agent_folder_path, downloaded_folder_name, "core", "cat", "plugins"))
 shutil.copytree(os.path.join(agent_folder_path,modified_files_path, "plugins"), os.path.join(agent_folder_path, downloaded_folder_name, "core", "cat", "plugins"))
-
-
-# Costruiamo il percorso del file pyproject.toml all'interno della cartella
-pyproject_toml_path = os.path.join(agent_folder_path, "agente_base", "core", "pyproject.toml")
-
-# Leggi la versione da pyproject.toml
-try:
-    with open(pyproject_toml_path, 'r') as file:
-        pyproject_toml_content = toml.load(file)
-        version_string = pyproject_toml_content["project"].get("version")
-except FileNotFoundError:
-    print(f"Errore: Il file {pyproject_toml_path} non è stato trovato.")
-    version_string = None
-except KeyError:
-    print("Errore: La chiave 'version' non è presente nel file pyproject.toml.")
-    version_string = None
-
-# Verifica se la versione è maggiore di 1.4.8
-if version_string and version.parse(version_string) == version.parse("1.4.8"):
-    print('La versione è coretta.')
-    # Sostituisci i file in looking_glass
-    looking_glass_path = os.path.join(agent_folder_path, downloaded_folder_name, "core", "cat", "looking_glass")
-    shutil.copy(os.path.join(agent_folder_path, modified_files_path, "agent_manager.py"), os.path.join(looking_glass_path, "agent_manager.py"))
-    shutil.copy(os.path.join(agent_folder_path, modified_files_path, "stray_cat.py"), os.path.join(looking_glass_path, "stray_cat.py"))
-else:
-    print('La versione è troppo nuova per caricare i file migliorati.')
 
 
 # Costruiamo il percorso del file docker-compose.yml all'interno della cartella
@@ -70,8 +46,8 @@ with open(percorso_docker_compose, 'r') as file:
 # Modifichiamo il contenuto come necessario
 docker_compose_content['services']['cheshire-cat-core']['container_name'] = f"cheshire_cat_core_base"
 # Per modificare la porta (decommenta e modifica se necessario)
-# docker_compose_content['services']['cheshire-cat-core']['environment'][3] = f"CORE_PORT={1865}"
-# docker_compose_content['services']['cheshire-cat-core']['ports'][0] = f"{1865}:80"
+docker_compose_content['services']['cheshire-cat-core']['environment'][3] = f"CORE_PORT={1866}"
+docker_compose_content['services']['cheshire-cat-core']['ports'][0] = f"{1866}:80"
 
 # Sovrascrivi il file docker-compose.yml con le modifiche
 with open(percorso_docker_compose, 'w') as file:
